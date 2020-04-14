9b703849dd91b0f3f77b1aec051069ce3c31dfb7
Samuel6311 <56605188+Samuel6311@users.noreply.github.com>
2020-04-14 14:31:11 -0500

    Select and update/insert SM

---
+++ Student Success Planner/Data/DatabaseSelect.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Student_Success_Planner.Data
+{
+    public class DatabaseSelect
+    {
+        private string Statement = "";
+
+        //Selects all College table information from the database
+        public void SelectColleges()
+        {
+            Statement = "SELECT * FROM College";
+        }
+
+        //Selects all Department table information with a CollegeID
+        public void SelectDepartments(int CID)
+        {
+            Statement = "Select * FROM Department WHERE CollegeID =" + CID;
+        }
+
+        //Selects all Program table information with a DepartmentID
+        public void SelectProgram(int DID)
+        {
+            Statement = "Select * FROM Program WHERE DeptID =" + DID;
+        }
+
+        //Selects all SuccessMap table information with a ProgramID
+        public void SelectSuccessMap(int PID)
+        {
+            Statement = "Select * FROM SuccessMap WHERE PrgmID =" + PID;
+        }
+
+        //Objective ID to select Objective info
+        public void SelectObjective(int OID)
+        {
+            Statement = "Select * FROM Objective WHERE ObjectiveID =" + OID;
+        }
+
+        //Select all years
+        public void SelectAllYears()
+        {
+            Statement = "Select * FROM Year";
+        }
+
+        //Year to select Year info
+        public void SelectYear(int Yr)
+        {
+            Statement = "Select * FROM Year WHERE Year =" + Yr;
+        }
+
+        //Year to select semesters
+        public void SelectSemester(int Yr)
+        {
+            Statement = "Select * FROM  Semester WHERE Year =" + Yr;
+        }
+
+        //Success Category ID to select Success Category info
+        public void SelectCategory(int CID)
+        {
+            Statement = "Select * FROM SuccessCategory WHERE CategoryID =" + CID;
+        }
+
+        //Objective ID to select Success Objective map
+        public void SelectSuccessObjMapObjective(int OID)
+        {
+            Statement = "Select * FROM SuccessObjectiveMapping WHERE ObjID =" + OID;
+        }
+
+        //Semester ID to select Success Objective map
+        public void SelectSuccessObjMapSemester(int SID)
+        {
+            Statement = "Select * FROM SuccessObjectiveMapping WHERE SMID =" + SID;
+        }
+
+    }
+}
---
+++ Student Success Planner/Data/DatabaseUpdate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Student_Success_Planner.Data
+{
+    public class DatabaseUpdate
+    {
+        private string Statement = "";
+
+        public void UpdateSM(int SID, String SName, int PID, string Abr)
+        {
+            Statement = "UPDATE SuccessMap SET " +
+                        "SMName = " + SName +
+                        ", PrgmID = " + PID +
+                        ", Abbr = " + Abr +
+                        "WHERE SMID = " + SID;
+        }
+
+        public void InsertSM(String SName, int PID, string Abr)
+        {
+            Statement = "INSERT INTO SuccessMap(SMName, PrgmID, Abbr) " +
+                        "VALUES('"+SName+"','"+PID+"','"+Abr+"')";
+        }
+    }
+}
